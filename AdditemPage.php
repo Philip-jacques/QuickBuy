7c7f06054e0e5e257fa6b260bb9a3a9f65fb62b3
Philip-jacques <pfourie517@gmail.com>
2025-06-08 14:48:13 +0200

    Update AdditemPage.php

--- AdditemPage.php
+++ AdditemPage.php
@@ -455,6 +455,15 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'seller') {
             }
         }
     </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M37ZFNLZ9Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M37ZFNLZ9Q');
+</script>
 </head>
 <body>
 
