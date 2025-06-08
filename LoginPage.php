c43818ded33381766074769f0c7c38e5a9fa4168
Philip-jacques <pfourie517@gmail.com>
2025-06-08 14:52:08 +0200

    Update LoginPage.php

--- LoginPage.php
+++ LoginPage.php
@@ -598,6 +598,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
     </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M37ZFNLZ9Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M37ZFNLZ9Q');
+</script>
+
 </head>
 <body>
 
