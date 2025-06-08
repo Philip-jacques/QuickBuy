bac56295115d791bdf609d68cd626d65e06cf8b4
Philip-jacques <pfourie517@gmail.com>
2025-06-08 15:16:03 +0200

    Update manageAdmin.php

--- manageAdmin.php
+++ manageAdmin.php
@@ -705,6 +705,15 @@ $results = $admins->get_result();
             }
         }
     </style>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M37ZFNLZ9Q"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-M37ZFNLZ9Q');
+</script>
 </head>
 <body>
 
